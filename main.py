ee06eedd5834c192fb18db6cb7056b14ba4e4a94
RafalKodland <168269766+RafalKodland@users.noreply.github.com>
2024-09-20 17:57:09 +0200

    Zmiana kodu na bota

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-meme_dict = {
-    "LOL": "odpowiedź na coś zabawnego",
-    "CRINGE": "coś dziwnego lub wstydliwego",
-    "ROFL": "odpowiedź na żart",
-    "SHEESH": "lekka dezaprobata",
-    "CREEPY": "straszny, złowieszczy",
-    "AGGRO": "stać się agresywnym/zły"
-}
+import discord
+from discord.ext import commands
+from gen_pass import gen_pass
 
-while True:
-    word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("Tego słowa nie ma w słowniku")
-    print(" ")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def haslo(ctx, count_char = 10):
+    await ctx.send(gen_pass(count_char))
+
+bot.run("")
